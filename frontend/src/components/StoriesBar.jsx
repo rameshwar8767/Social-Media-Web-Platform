260072b8a47bdc7b5817ec92b8f6f00c4e8490c2
rameshwar mane <manerameshwar909@gmail.com>
2026-01-10 13:01:53 +0530

    feat: add initial frontend structure with assets, components, and pages
    - Created assets including images and SVGs for logo, background, and sample profiles.
    - Implemented core components: Loading, MenuItems, SideBar, and StoriesBar.
    - Developed main pages: Feed, Profile, Messages, Connections, Discover, CreatePost, ChatBox, and Login.
    - Integrated routing with React Router and Clerk for authentication.
    - Added Tailwind CSS for styling and layout.
    - Set up Vite as the build tool for the frontend application.

---
+++ frontend/src/components/StoriesBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StoriesBar = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default StoriesBar
---
+++ frontend/src/pages/Feed.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react'
+import { dummyPostsData } from '../assets/assets'
+import Loading from '../components/Loading'
+
+const Feed = () => {
+
+  const [feeds,setFeeds] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  const fetchFeeds = async()=>{
+    setFeeds(dummyPostsData)
+    setLoading(false)
+  }
+
+  useEffect(()=>{
+    fetchFeeds()
+  },[])
+
+  return !loading ? (
+    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+       {/* Stories and post */}
+       <div>
+        <h1>Stories here</h1>
+        <div className='p-4 space-y-6'>
+          List of post
+        </div>
+       </div>
+
+       {/* Right SideBar */}
+       <div>
+        <div>
+          <h1>Sponsored</h1>
+        </div>
+
+        <div>
+          <h1>Recent messages</h1>
+        </div>
+        
+       </div>
+    </div>
+  ) : <Loading/>
+}
+
+export default Feed
