93396cd37e2437d452000236469714826c66a64f
rameshwar mane <manerameshwar909@gmail.com>
2026-01-12 18:07:07 +0530

    feat: add ProfileModel component with user data integration

---
+++ frontend/src/components/ProfileModel.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { dummyUserData } from '../assets/assets'
+
+const ProfileModel = () => {
+
+    const user = dummyUserData;
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProfileModel
